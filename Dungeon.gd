99ee237be71f0954fdfd93139dc11d81fbcc94cf
Griffinhale <127063646+Griffinhale@users.noreply.github.com>
2024-08-21 15:37:16 -0700

    added test_material

--- Dungeon.gd
+++ Dungeon.gd
@@ -1,15 +1,84 @@
 extends Node2D
 
 class_name Dungeon
+# Add a switch to map generators based off string in element
+# This will allow for different tiles and materials from the tileset for each dungeon
+var element: String
+var boss: PackedScene
+var enemy_types: Array
+var resources_available: Array
+var tilemap_base: TileMapLayer
+var tilemap_materials: TileMapLayer
+var tilemap_cover: TileMapLayer
+var room_size: Vector2 = Vector2(25,25)
+var layout: Array = []
 
-var element
-var boss
-var enemy_types
-var resources_available
+@onready var tileset = load("res://isoset.tres")
+@onready var mat_script = load("res://tes_material.gd")
 
-func generate_layout():
-	pass
+
+func _ready():
+	generate_layout()
+	generate_materials()
+	generate_cover()
 	
+func generate_layout():
+	layout = []
+	for x in range(room_size.x):
+		var row = []
+		for y in range(room_size.y):
+			var tile_type = choose_tile_type(x,y)
+			row.append(tile_type)
+		layout.append(row)
+	apply_layout_to_tilemap()
+
+func choose_tile_type(x: int, y: int) -> int:
+	if x == 0 or y == 0 or x == room_size.x - 1 or y == room_size.y - 1:
+		return 1 # wall
+	return 0 # floor
+	
+func apply_layout_to_tilemap():
+	if not tilemap_base:
+		tilemap_base = TileMapLayer.new()
+		tilemap_base.set_tile_set(tileset)
+		add_child(tilemap_base)
+	
+	tilemap_base.clear()
+	for x in range(room_size.x):
+		for y in range(room_size.y):
+			var tile_type = layout[x][y]
+			if tile_type == 0:
+				tilemap_base.set_cell(Vector2(x, y), 0, Vector2(randi_range(0,5), 0))
+			else:
+				tilemap_base.set_cell(Vector2(x, y), 0, Vector2(randi_range(0,5), 7))
+
+func generate_materials():
+	if not tilemap_materials:
+		tilemap_materials = TileMapLayer.new()
+		tilemap_materials.set_tile_set(tileset)
+		add_child(tilemap_materials)
+		
+	for x in range(room_size.x):
+		for y in range(room_size.y):
+			if layout[x][y] == 0 and randf() < 0.03:
+				tilemap_materials.set_cell(Vector2(x,y), 0, Vector2(0,4))
+				print("test")
+				var material_node = Area2D.new()
+				material_node.set_script(mat_script)
+				var tile_world_pos = tilemap_materials.map_to_local(Vector2(x,y))
+				material_node.global_position = to_global(tile_world_pos)
+				var collision_shape = CollisionShape2D.new()
+				collision_shape.shape = CircleShape2D.new()
+				collision_shape.debug_color = Color(255,255,255,1)
+				collision_shape.shape.radius = 10
+				material_node.add_child(collision_shape)
+				material_node.add_to_group("interactable")
+				print(material_node.position, material_node.global_position)
+				add_child(material_node)
+
+func generate_cover():
+	pass
+
 func spawn_enemies():
 	pass
 
--- Summoner.gd
+++ Summoner.gd
@@ -3,10 +3,20 @@ extends BaseEntity
 class_name Summoner
 
 @onready var test_smn = preload("res://test_summon.tscn")
+#interact vars
+signal interact_obj
+var interaction_range = 50
 
+#game vars
 var pouch_mixes
 var active_summons
 var inventory
+
+#movement vars
+var velocity = Vector2.ZERO
+var max_speed = 200
+var base_acceleration = 800
+var friction = 400
 
 func summon_monster():
 	pass
@@ -17,21 +27,51 @@ func switch_loadout():
 func heal():
 	pass
 
+func _on_interact_obj():
+	var closest_interactable = null
+	var closest_distance = interaction_range
+	print("signal emitted")
+	# Iterate through all objects `in the "interactable" group
+	for obj in get_tree().get_nodes_in_group("interactable"):
+		var distance = position.distance_to(obj.position)
+		print(distance)
+		if distance < closest_distance:
+			print("found one")
+			closest_distance = distance
+			closest_interactable = obj
+
+	# If there's a nearby interactable object, trigger its interaction method
+	if closest_interactable:
+		closest_interactable.interact_obj()
+
+
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			var m = test_smn.instantiate()
-			add_child(m)
-	if event is InputEventKey:
-		if event.is_action("ui_down"):
-			position.x += 1
-			position.y += 1
-		if event.is_action("ui_left"):
-			position.x -= 1
-			position.y += 1
-		if event.is_action("ui_up"):
-			position.x -= 1
-			position.y -= 1
-		if event.is_action("ui_right"):
-			position.x += 1
-			position.y -= 1
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("ui_accept"):
+		print("huh?")
+		interact_obj.emit()
+	if Input.is_action_pressed("ui_up"):
+		input_vector.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("ui_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("ui_left"):
+		input_vector.x -= 1
+
+	input_vector = input_vector.rotated(PI / 4).normalized()
+
+	if input_vector != Vector2.ZERO:
+		var dynamic_acceleration = base_acceleration * (1 - velocity.length() / max_speed)
+		velocity += input_vector * dynamic_acceleration
+
+	# Clamp the velocity to the maximum speed
+	velocity = velocity.limit_length(max_speed)
+
+func _process(delta):
+	# Apply friction to slow down the character when no input is given
+	if velocity.length() > 0:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+
+	# Update position based on velocity
+	position += velocity * delta
