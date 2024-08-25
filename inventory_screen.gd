ed1fbf78c29ef297da691c6bf6595184a0a8c97d
Griffinhale <127063646+Griffinhale@users.noreply.github.com>
2024-08-25 12:43:53 -0700

    started dict for inventory icons

--- inventory_screen.gd
+++ inventory_screen.gd
@@ -1,15 +1,26 @@
 extends CanvasLayer
 
-
+var flower_icon: Resource = preload("res://flower_icon.tres")
+var keyword_icons: Dictionary
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	
+	keyword_icons = {
+		"flower": flower_icon
+	}
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func update_inventory(player_inventory: Dictionary):
 	print("should update slot to have correct icon for materials keyword (this is where material class comes into play??")
-	
+	if !player_inventory.is_empty():
+		for i in player_inventory.keys():
+			var idx = player_inventory.keys().bsearch(i)
+			var slot_for_item = $Background/ItemSlots.get_child(idx)
+			print(slot_for_item.name + " " + flower_icon.resource_name)
+			if slot_for_item.get_child_count() == 0:
+				var img = TextureRect.new()
+				img.texture = flower_icon
+				print(img.texture)
+				slot_for_item.add_child(img)
